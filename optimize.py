5c80c4950d1d92d08524321ddf4d524aad330d22
Aemil Estvold <mrnitrate@gmail.com>
2015-10-22 18:41:22 -0400

    the player with the least dropoff is now removed between optimal rosters instead of by risk;  added qb stacking, max of 2 offensive players per game constraints, set proj to projected ceiling for better GPP rosters

--- optimize.py
+++ optimize.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 # A huge thanks to swanson
 # this solution is almost wholly based off
 # https://github.com/swanson/degenerate
@@ -38,7 +40,7 @@ def check_missing_players(all_players, min_cost, e_raise):
 		raise Exception('Total missing players at price point: ' + str(miss))
 
 
-def run_solver(solver, all_players, max_flex):
+def run_solver(solver, all_players):
 	'''
 	handle or-tools logic
 	'''
@@ -64,7 +66,7 @@ def run_solver(solver, all_players, max_flex):
 	'''
 	Add Salary Cap Constraint
 	'''
-	salary_cap = solver.Constraint(SALARY_CAP-5000, SALARY_CAP)
+	salary_cap = solver.Constraint(SALARY_CAP-1000, SALARY_CAP)
 	for i, player in enumerate(all_players):
 		salary_cap.SetCoefficient(variables[i], player.cost)
 
@@ -78,7 +80,7 @@ def run_solver(solver, all_players, max_flex):
 
 	solver.Add(flex_rb+flex_wr+flex_te==1)
 
-	for position, limit in max_flex:
+	for position, limit in POSITION_LIMITS_FLEX:
 		ids, players_by_pos = zip(*filter(lambda (x,_): x.pos in position, zip(all_players, variables)))
 		if position == 'WR':
 			  solver.Add(solver.Sum(players_by_pos) == limit+flex_wr)
@@ -103,12 +105,50 @@ def run_solver(solver, all_players, max_flex):
 		ids, players_by_team = zip(*filter(lambda (x,_): x.team in team, zip(all_players, variables)))
 		solver.Add(teams[idx]<=solver.Sum(players_by_team))
 
-	# o_players = filter(lambda x: x.pos in ['QB','WR','RB','TE'], all_players)
-	# opps_team_names= set([o.opps_team for o in o_players])
-	# opps_teams = []
-	# for idx,team in enumerate(opps_team_names):
-		# opps_teams.append(solver.IntVar(0,idx,'opps_'+team))
-	# print(opps_teams)
+
+	'''
+	Add Defense cant play against any offense's player team constraint
+	'''
+
+	o_players = filter(lambda x: x.pos in ['QB','WR','RB','TE'], all_players)
+	opps_team_names= set([o.opps_team for o in o_players])
+	teams_obj = filter(lambda x: x.pos == 'DST' , all_players)
+	teams = set([o.team for o in teams_obj])
+
+	# for opps_team in team_names:
+		# if opps_team in teams :
+			# ids, players_by_opps_team = zip(*filter(lambda (x,_): x.pos in ['QB','WR','RB','TE'] and x.opps_team in opps_team, zip(all_players, variables)))
+			# idxs, defense = zip(*filter(lambda (x,_): x.pos == 'DST' and x.team in opps_team, zip(all_players, variables)))
+			# solver.Add(solver.Sum(players_by_opps_team)<=solver.Sum(defense))
+
+
+	'''
+	Add QB stacking (at least 1 wr or te on same team as QB) constraint
+	'''
+ 	offense_team_names = set([o.team for o in o_players])
+	for o_team in offense_team_names:
+ 		ids, players_by_team = zip(*filter(lambda (x,_): x.pos in ['WR','TE'] and x.team == o_team, zip(all_players, variables)))
+ 		idxs, qb = zip(*filter(lambda (x,_): x.pos == 'QB' and x.team == o_team, zip(all_players, variables)))
+ 		solver.Add(solver.Sum(players_by_team)>=solver.Sum(qb))
+
+	'''
+	Add Max of 2 qb wr te or rb per game constraint
+	'''
+	for team in list(team_names)[:len(team_names)/2]:
+		team_players = filter(lambda x: x.team in team, all_players)
+		ids, players_by_game = zip(*filter(lambda (x,_): x.team in team or x.team in team_players[0].opps_team and x.pos in ['WR','TE','RB','QB'], zip(all_players, variables)))
+
+		solver.Add(solver.Sum(players_by_game)<=2)
+
+	'''
+	Add Max of 1 qb wr te or rb per team constraint
+	'''
+# 	for team in list(team_names):
+# 		ids, players_by_team = zip(*filter(lambda (x,_): x.team in team and x.pos in ['WR','TE','RB','QB'], zip(all_players, variables)))
+# 		solver.Add(solver.Sum(players_by_team)<=1)
+
+
+
 
 #	  for position, limit in max_flex:
 #		  position_cap = solver.Constraint(limit, limit)
@@ -130,8 +170,7 @@ def load(all_players):
 			if idx > 0:
 				pname = row[5]+" "+row[1]
 				pname = pname.replace(".",'').replace("-",'').replace(" ","").lower()
-				all_players.append(Player(row[0],row[1],row[2],
-									row[3],row[5],0,0,pname))
+				all_players.append(Player(row[0],row[1].strip(),row[2],row[3],row[5],0,0,pname))
 
 	# give each a ranking
 	all_players = sorted(all_players, key=lambda x: x.cost, reverse=True)
@@ -140,39 +179,36 @@ def load(all_players):
 
 	with open('data/FFA-CustomRankings.csv', 'rb') as csvfile:
 		csvdata = csv.DictReader(csvfile)
-		mass_hold = [['playername', 'points', 'cost', 'ppd']]
 
 		for row in csvdata:
 			pname=row['team'].lower()+row['playername'].replace(".",'').replace("-",'').replace(" ","").lower()
-
-			if row['points'] == 0 :
-				points = 0
-			else:
-				points = int(repr(float(row['points'])*10).split('.')[0])
-
-			if row['risk'] =='null' :
-				risk = 0
-			else:
-				risk = int(repr(float(row['risk'])*10).split('.')[0])
-
-			holder = row
-
 			player = filter(lambda x: x.code in pname, all_players)
 			try:
+				if row['upper'] == 0 :
+					points = 0
+				else:
+					points = int(repr(float(row['upper'])*100).split('.')[0])
+
+				if row['dropoff'] == 'null' :
+					dropoff = 0
+				else:
+					dropoff = int(repr(float(row['dropoff'])).split('.')[0])
+
+				if row['risk'] =='null' :
+					risk = 0
+				else:
+					risk = int(repr(float(row['risk'])*100).split('.')[0])
+
+				if '0.0' not in row['upper'] or player[0].cost != 0:
+					ppd = (float(row['upper'])*100) / float(player[0].cost)
+				else:
+					ppd = 0	
+						
 				player[0].proj = points
 				player[0].risk = risk
 				player[0].marked = 'Y'
-				listify_holder = [
-					pname,
-					points
-				]
-				if '0.0' not in row['points'] or player[0].cost != 0:
-					ppd = (float(row['points'])*10) / float(player[0].cost)
-				else:
-					ppd = 0
-				listify_holder.extend([player[0].cost,
-									   ppd * 100000])
-				mass_hold.append(listify_holder)
+				player[0].dropoff = dropoff
+
 			except Exception as e:
 				pass
 				#print(e,pname,player)
@@ -183,11 +219,11 @@ def load(all_players):
 '''
 Removed previous optimal solution and run another iteration
 '''
-def run(all_players,max_flex, maxed_over):
+def run(all_players, maxed_over):
 	solver = pywraplp.Solver('FD',
 							 pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
 
-	variables, solution = run_solver(solver, all_players, max_flex)
+	variables, solution = run_solver(solver, all_players)
 
 	if solution == solver.OPTIMAL:
 		roster = Roster()
@@ -196,7 +232,7 @@ def run(all_players,max_flex, maxed_over):
 			if variables[i].solution_value() == 1:
 				roster.add_player(player)
 
-		print "Optimal roster for: %s" % maxed_over
+		print "Optimal roster iterations: %s" % maxed_over
 		print roster
 		return roster
 	else:
@@ -209,13 +245,26 @@ Main Loop
 if __name__ == "__main__":
 	load(all_players)
 	rosters = []
+
 	for x in xrange(0, int(args.i)):
 		remove = []
-		rosters.append(run(all_players,POSITION_LIMITS_FLEX, 'flex'))
+		rosters.append(run(all_players,x+1))
+		#for roster in rosters:
+		for player in rosters[-1].players:
+			remove.append(player.code)
+		remove_players = filter(lambda x: x.code in remove  , all_players)
+		sorted_remove = sorted(remove_players, key=lambda x: x.dropoff, reverse=False)
+		# risk =  sorted(remove_players, key=lambda x: x.risk, reverse=True)
+		# risk  = frozenset(risk[:5])
+		# combined = [x for x in sorted_remove[:5] if x in risk]
+		# if not combined:
+			# combined.add(sorted_remove[0])
+
+		bad =  next(iter(sorted_remove)).code
+		all_players = filter(lambda x: x.code not in bad, all_players)
+
+	with open('data/bulk-import.csv', 'wb') as csvfile:
+		writer = csv.writer(csvfile,delimiter=',',quotechar='"',quoting=csv.QUOTE_NONNUMERIC)
 		for roster in rosters:
-			for player in roster.players:
-				remove.append(player.code)
-			remove_players = filter(lambda x: x.code in remove, all_players)
-			sorted_remove = sorted(remove_players, key=lambda x: x.risk, reverse=True)
-			bad = [p.code for p in sorted_remove[:1]]
-			all_players = filter(lambda x: x.code not in bad, all_players)
+			writer.writerow([x.name for x in roster.sorted_players()])
+
--- orm.py
+++ orm.py
@@ -3,89 +3,99 @@
 from constants import ALL_POS_TEAM
 
 class Roster:
-    POSITION_ORDER = {
-        "QB": 0,
-        "RB": 1,
-        "WR": 2,
-        "TE": 3,
-        "DST": 4
-    }
+	POSITION_ORDER = {
+		"QB": 0,
+		"RB": 1,
+		"WR": 2,
+		"TE": 3,
+		"DST": 4
+	}
 
-    def __init__(self):
-        self.players = []
+	def __init__(self):
+		self.players = []
 
-    def add_player(self, player):
-        self.players.append(player)
+	def add_player(self, player):
+		self.players.append(player)
 
-    def spent(self):
-        return sum(map(lambda x: x.cost, self.players))
+	def spent(self):
+		return sum(map(lambda x: x.cost, self.players))
 
-    def projected(self):
-        return sum(map(lambda x: x.proj, self.players))/10
+	def projected(self):
+		return sum(map(lambda x: x.proj, self.players))/100
 
-    def position_order(self, player):
-        return self.POSITION_ORDER[player.pos]
+	def position_order(self, player):
+		return self.POSITION_ORDER[player.pos]
 
-    def sorted_players(self):
-        return sorted(self.players, key=self.position_order)
+	def sorted_players(self):
+		sortedp = sorted(self.players, key=self.position_order)
+		rbs = filter(lambda x: x.pos=='RB', self.players )
+		wrs = filter(lambda x: x.pos=='WR', self.players )
 
-    def __repr__(self):
-        s = '\n'.join(str(x) for x in self.sorted_players())
-        s += "\nProjected Score: %s" % self.projected()
-        s += "\tCost: $%s\n" % self.spent()
-        return s
+		if (len(rbs)>2):
+			sortedp.insert(7,sortedp.pop(3))
+		elif (len(wrs)>3):
+			sortedp.insert(7,sortedp.pop(6))
+
+		return sortedp
+
+	def __repr__(self):
+		s = '\n'.join(str(x) for x in self.sorted_players())
+		s += "\nProjected Score: %s" % self.projected()
+		s += "\tCost: $%s\n" % self.spent()
+		return s
 
 class Player:
-    def __init__(self, pos, name, cost,
-	    		matchup=None, team=None, risk=0, proj=0, code='aa', marked=None):
-        self.pos = pos
-        self.name = name
-        self.code = code
-        self.team = team
-        self.matchup = matchup
-        self.opps_team = matchup.split()[0].replace(team,'').replace('@','')
-        self.cost = int(cost)
-        self.risk = risk
-        self.proj = proj
-        self.marked = marked
-        self.cost_ranking = 0
+	def __init__(self, pos, name, cost,
+				matchup=None, team=None, risk=0, proj=0, code='aa', marked=None):
+		self.pos = pos
+		self.name = name
+		self.code = code
+		self.dropoff = 0
+		self.team = team
+		self.matchup = matchup
+		self.opps_team = matchup.split()[0].replace(team,'').replace('@','')
+		self.cost = int(cost)
+		self.risk = risk
+		self.proj = proj
+		self.marked = marked
+		self.cost_ranking = 0
 
-    def __repr__(self):
-        return "[{0: <2}] {1: <20} {2} {3} (${4}, {5})".format(self.pos, \
-                                    self.name, \
+	def __repr__(self):
+		return "[{0: <2}] {1: <20} {2} {3} (${4}, {5})".format(self.pos, \
+									self.name, \
 									self.team, \
-                                    self.matchup, \
-                                    self.cost, \
-                                    self.proj/10)
+									self.matchup, \
+									self.cost, \
+									self.proj/100)
 
 class Team:
-    def __init__(self, give):
-        self._set_team_pos(give)
-        self.team_cost = self._get_team_prop('cost')
-        self.team_proj = self._get_team_prop('proj')
+	def __init__(self, give):
+		self._set_team_pos(give)
+		self.team_cost = self._get_team_prop('cost')
+		self.team_proj = self._get_team_prop('proj')
 
-    def team_report(self):
-        for pos in ALL_POS_TEAM:
-            getattr(self, pos).player_report()
+	def team_report(self):
+		for pos in ALL_POS_TEAM:
+			getattr(self, pos).player_report()
 
-        print 'Total Cost: ' + str(self.team_cost)
-        print 'Total Projected: ' + str(self.team_proj)
+		print 'Total Cost: ' + str(self.team_cost)
+		print 'Total Projected: ' + str(self.team_proj)
 
-    def contains_dups(self):
-        players = []
-        for pos in ALL_POS_TEAM:
-            name = getattr(self, pos).name
-            players.append(name)
+	def contains_dups(self):
+		players = []
+		for pos in ALL_POS_TEAM:
+			name = getattr(self, pos).name
+			players.append(name)
 
-        return len(players) != len(set(players))
+		return len(players) != len(set(players))
 
-    def _set_team_pos(self, give):
-        for idx, val in enumerate(give):
-            setattr(self, ALL_POS_TEAM[idx], val)
+	def _set_team_pos(self, give):
+		for idx, val in enumerate(give):
+			setattr(self, ALL_POS_TEAM[idx], val)
 
-    def _get_team_prop(self, prop):
-        val = 0
-        for pos in ALL_POS_TEAM:
-            val += int(getattr(getattr(self, pos), prop))
+	def _get_team_prop(self, prop):
+		val = 0
+		for pos in ALL_POS_TEAM:
+			val += int(getattr(getattr(self, pos), prop))
 
-        return val
+		return val
